cd022a401f5ad6ebf2ae8b8406933a8648f234ab
qitan <youhao.tyh@alibaba-inc.com>
2016-12-01 23:37:31 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// 访问首页
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: '欢迎段金粒'
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+// 跳转到当前用户的页面
+router.get('/:id', function(req, res, next) {
+    res.send('respond with a resource');
 });
+
+// 用户注册
+router.post('/reg', function(req, res, next) {
+    res.send('用户注册成功!');
+});
+
+// 用户发表blog 
+router.post('/post', function(req, res, next) {
+
+});
+
+// 跳转到用户登录页面
+router.get('/login', function(req, res, next) {
+
+});
+
+// 用户登录
+router.post('/doLogin', function(req, res, next) {
+
+});
+
+// 用户登出
+router.get('/loginout', function(req, res, next) {
+
+});
+
 
 module.exports = router;
